c5f7822c38882f5cc23197add14ed4bcb43403b9
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2023-10-03 08:20:17 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -9,13 +9,13 @@ def buildImage() {
 
     }
 
-def deployImage() {
-    echo "deploying image to ACR ...."
-    withCredentials([usernamePassword(credentialsId: 'azure_acr_cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin nedumacr.azurecr.io"
-        sh 'docker push nedumacr.azurecr.io/demo-app:jma-${BUILD_NUMBER}'
-    }
-}
+// def deployImage() {
+//     echo "deploying image to ACR ...."
+//     withCredentials([usernamePassword(credentialsId: 'azure_acr_cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh "echo $PASS | docker login -u $USER --password-stdin nedumacr.azurecr.io"
+//         sh 'docker push nedumacr.azurecr.io/demo-app:jma-${BUILD_NUMBER}'
+//     }
+// }
 def deployApp() {
     echo 'deploying the application...'
 } 
